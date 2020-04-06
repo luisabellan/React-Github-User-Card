2031151eecb1467ed6c5aac07e1a182cc222df2a
squarerobin <luis.abellan.dev@gmail.com>
2020-04-06 06:36:25 +0200

    save

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -5,34 +5,15 @@ import Cards from "./Cards/Cards";
 
 class App extends React.Component {
   constructor(props) {
-    console.log('constructor is getting called')
-    super(props)
+    console.log("constructor is getting called");
+    super(props);
     this.state = {
-      follower: {
-
-        name: "",
-        image: "",
-        userName: "luisabellan",
-        location: "",
-        url: "",
-        followers: "",
-        following: "",
-        bio: "",
-
-      },
-      followersArray:[]  
-    
-
-      
-
      
-
-      
-    }
+      followersArray: []
+    };
   }
 
-
- 
+  
 
   render() {
     //console.log(this.state.user)
@@ -41,7 +22,7 @@ class App extends React.Component {
       <div className="container">
         <div className="header">
           <div className="left-logo">
-            <img className="lambda-logo" alt="Lambda Logo" />
+            <img className="lambda-logo" src="" alt="Lambda Logo" />
           </div>
           <p>
             <span aria-label="heart" role="img">
@@ -49,10 +30,14 @@ class App extends React.Component {
             </span>
           </p>
           <div className="right-logo">
-            <img className="github-logo" alt="GitHub Logo" />
+            <img
+              className="github-logo"
+              src={`./assets/lambdalogo.png`}
+              alt="GitHub Logo"
+            />
           </div>
         </div>
-        <Card userName={this.state.follower.userName}/>
+        <Card className="card" userName= "luisabellan" />
         <Cards className="cards" followers={this.state.followersArray} />
       </div>
     );
--- react-github-user-card/src/Cards/Cards.js
+++ react-github-user-card/src/Cards/Cards.js
@@ -1,72 +1,50 @@
 import React from 'react'
-import axios from 'axios'
+import axios from 'axios';
 import './Cards.css';
 
 class Card extends React.Component {
-    
-    
-    constructor(props) {
-        console.log('constructor is getting called')
-        super(props);
-        console.log(props);
-        
-        
+  constructor(props) {
+    console.log("constructor is getting called");
+    super(props);
+    console.log(props);
 
-        this.state = {
-            ...props.followersArray,
+    this.state = {
+     
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount is getting called");
+
+   
+    axios
+      .get(
+        `https://api.github.com/users/luisabellan/followers`
+      )
+      .then(res => {
+        // this probably returns a 200 status code
+        console.log(res.data);
+        this.setState({
+            followers : res.data
+        })
+      })
+
+     
             
-            followersArray: []
-        }
        
 
+      .catch(err => {
+        // this probably returns either a 400 or 500 status code
+        console.log("You hit an error: ", err);
+      });
+  
     }
-        
+  render() {
+    //console.log()
 
-    componentDidMount() {
-        console.log('componentDidMount is getting called')
-
-        axios
-            .get("https://api.github.com/users/luisabellan/followers")
-            .then(res => {
-                // this probably returns a 200 status code
-                console.log(res.data)
-                
-                res.data.map(follower => this.setState(
-                    
-
-                    <Card username={this.state.followersArray.userName}/>
-
-                    
-                    
-                    
-                ))
-                
-
-                
-            })
-
-
-
-
-            .catch((err) => {
-                // this probably returns either a 400 or 500 status code
-                console.log('You hit an error: ', err);
-            });
-
-
-
-
-    }
-
-    render() {
-        //console.log()
-
-      return this.state.followersArray.map(follower => <Card />)
-      
-        
-        
-        
-    }
+    return this.state.followers.map(follower => <Card userName={follower.userName} />);
+  }
 }
 
 
